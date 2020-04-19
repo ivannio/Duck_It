ec31cf0c71d672f08a1085a9221883a259a815a3
Ivan Phelps <ivanniothemannio@gmail.com>
2020-04-19 16:22:45 -0500

    updates as new items load

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,12 @@
-window.onload = () => {
-    let images = document.getElementsByTagName("img");
+const duckIt = () => {
+    let images = document.querySelectorAll("img");
 
     for (let i = 0; i < images.length; i++)
     {
         images[i].src = "https://raw.githubusercontent.com/ivannio/Duck_It/master/src/images/duck-large.png";
     }
+    setTimeout(duckIt, 2000);
+};
+window.onload = () => {
+    duckIt();
 }
