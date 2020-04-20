8c84a7057289f79dc779082261da7d8559215966
Ivan Phelps <ivanniothemannio@gmail.com>
2020-04-19 20:24:13 -0500

    twitch emotes no longer safe from duck it

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,11 @@
 const duckIt = () => {
     let images = document.querySelectorAll("img");
 
-    for (let i = 0; i < images.length; i++)
-    {
+    for (let i = 0; i < images.length; i++) {
         images[i].src = "https://raw.githubusercontent.com/ivannio/Duck_It/master/src/images/duck-large.png";
+        images[i].srcset = "";
     }
-    setTimeout(duckIt, 2000);
+    setTimeout(duckIt, 500);
 };
 window.onload = () => {
     duckIt();
